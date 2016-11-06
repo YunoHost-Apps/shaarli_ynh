9d73f6a7c0566ee8d53879ac76571118c6d871ff
lapineige <lapineige@lavache.com>
2016-11-06 15:32:35 +0100

    Fix user listing syntax
    Fix the error:
    > TypeError: string indices must be integers
    Caused by `print "{0}".format("\n".join(i["username"] for i in userlist))`
    `userlist` is a dictionary with {account :{dictionary of parameters}} so I use values() to return the dictionary of parameters.

--- conf/user_list.py
+++ conf/user_list.py
@@ -1,3 +1,3 @@
 import sys, json
 userlist=json.loads(sys.stdin.readlines()[0])["users"]
-print "{0}".format("\n".join(i["username"] for i in userlist))
+print "{0}".format("\n".join(i["username"] for i in userlist.values()))
