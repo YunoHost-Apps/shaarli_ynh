0999a92d53f3b642eae25361d09fb392ed31dca0
ericgaspar <junk.eg@free.fr>
2020-11-11 14:02:45 +0100

    Update _common.sh
    - Remove unused script that generates an error
    - Remove openssl that is part of PHP as default

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,10 +8,9 @@
 YNH_PHP_VERSION="7.3"
 
 # dependencies used by the app
-pkg_dependencies="openssl"
+pkg_dependencies=""
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gettext php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring"
-
 
 #=================================================
 # PERSONAL HELPERS
@@ -24,23 +23,3 @@ extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gettext 
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
- # Check if there's enough free space in a directory
-        is_there_enough_space () {
-                local free_space=$(df --output=avail "$1" | sed 1d)
-                test $free_space -ge $min_size
-        }
-
-        if is_there_enough_space /tmp; then
-                local tmpdir=/tmp
-        elif is_there_enough_space /var; then
-                local tmpdir=/var
-        elif is_there_enough_space /; then
-                local tmpdir=/
-        elif is_there_enough_space /home; then
-                local tmpdir=/home
-        else
-		ynh_die "Insufficient free space to continue..."
-        fi
-
-        echo "$(mktemp --directory --tmpdir="$tmpdir")"
-}
