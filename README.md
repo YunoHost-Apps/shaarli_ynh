33d286fcf932cad988bdf8d6aa1c46644a119962
lapineige <lapineige@users.noreply.github.com>
2020-11-30 09:43:42 +0100

    Testing : update to 0.12.0, nginx fix, other fixes (#58)
    * Set file permissions after creating the log (#53)
    
    Should fix #49
    
    * Updated to V0.12.0 and Nginx fix (#57)
    
    * Set file permissions after creating the log (#53) (#54)
    
    Should fix #49
    
    Authored-by: tituspijean <tituspijean@outlook.com>
    
    * Updated to version v0.12.0
    
    * Fix images
    
    * Fixed path in nginx
    
    Co-authored-by: lapineige <lapineige@users.noreply.github.com>
    Co-authored-by: anmol <anmol@jswan2.com>
    Co-authored-by: Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
    
    * Update README.md
    
    * rm -r -> ynh_secure_remove (#59)
    
    * Update issue_template.md
    
    Co-authored-by: Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
    
    * Update openssl dependency
    
    Co-authored-by: Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
    
    * Update _common.sh
    
    - Remove unused script that generates an error
    - Remove openssl that is part of PHP as default
    
    * Cleaning up
    
    * Fix linter warnings
    
    * Small typos (#60)
    
    * Patch2 (#61)
    
    - Fix NGINX 302 redirect
    - Fix missing upgrade_type in upgrade script
    - Add ynh_smart_mktemp helper
    
    * Fix NGINX
    
    * Re-implement automatic post-installation
    
    * Ensure file and directories permissions in upgrade
    
    * Replace all ynh_print_info with ynh_script_progression
    
    * Update check_process
    
    * Use permissions system
    
    Co-authored-by: tituspijean <tituspijean@outlook.com>
    Co-authored-by: anmol26s <5068843+anmol26s@users.noreply.github.com>
    Co-authored-by: anmol <anmol@jswan2.com>
    Co-authored-by: Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
    Co-authored-by: ericgaspar <junk.eg@free.fr>
    Co-authored-by: Alexandre Aubin <alex.aubin@mailoo.org>

--- README.md
+++ README.md
@@ -1,50 +1,72 @@
-# Shaarli app for Yunohost
----
+# Shaarli for YunoHost
 
-This app intends to provide Yunohost's users with Shaarli (https://github.com/shaarli/Shaarli), a simple but advanced bookmarking service.
+[![Integration level](https://dash.yunohost.org/integration/shaarli.svg)](https://dash.yunohost.org/appci/app/shaarli) ![](https://ci-apps.yunohost.org/ci/badges/shaarli.status.svg) ![](https://ci-apps.yunohost.org/ci/badges/shaarli.maintain.svg)  
+[![Install Shaarli with YunoHost](https://install-app.yunohost.org/install-with-yunohost.png)](https://install-app.yunohost.org/?app=shaarli)
 
-[![Integration level](https://dash.yunohost.org/integration/shaarli.svg)](https://dash.yunohost.org/appci/app/shaarli)  
-[![Install REPLACEBYYOURAPP with YunoHost](https://install-app.yunohost.org/install-with-yunohost.png)](https://install-app.yunohost.org/?app=shaarli)
+> *This package allows you to install Shaarli quickly and simply on a YunoHost server.
+If you don't have YunoHost, please consult [the guide](https://yunohost.org/#/install) to learn how to install it.*
 
-![UI](https://framapic.org/ui93xwvieFC5/bdLqopk0JqVv)
-It is based on the community version of Shaarli (from @sebsauvage original software).
-You can get a demo [here](https://shaarli.readthedocs.io/en/master/#demo).
+## Overview
+[Shaarli](https://github.com/shaarli/Shaarli) intends to provide YunoHost's users with a simple but advanced bookmarking service.
 
+**Shipped version:** 0.12.0
 
-Current version: **0.10.4**
+## Screenshots
 
-Maintainer: @Lapineige
+![](https://i.imgur.com/zGF4d6L.jpg)
 
-## This app claims following features
+![](https://i.imgur.com/27wYsbC.png)
 
-- [X] Install / remove scripts
-- [X] Backup/restore scripts
-- [X] Upgrade Script
-- [X] Change url
-- [X] Integrate fail2ban for the app
+## Demo
 
-## Installation information
+* [Official demo](https://demo.shaarli.org/)
 
-During the installation app will prompt you to install itself as public or private, if you choose public then anyone (not just Yunohost users) would be able to access your Shaarli instance.
+## Configuration
+
+During the installation app will prompt you to install itself as public or private, if you choose public then anyone (not just YunoHost users) would be able to access your Shaarli instance.
 
 After the installation is complete, go to the domain where the Shaarli is installed and create your account. After the creation of the account the registration will be locked.
 
-## TODO/Limitations
-    
-- [ ] Integrate the LDAP/SSOWAT authentication (currently the authentication is done by Shaarli)
+## Documentation
 
+ * Official documentation: https://shaarli.readthedocs.io/en/master/
+ * YunoHost documentation: If specific documentation is needed, feel free to contribute.
+ 
+## TODO/Limitations
+
+- [ ] Integrate the LDAP/SSOWAT authentication
 
 ## Changelog
 
 For Shaarli changelog, see: https://github.com/shaarli/Shaarli/blob/master/CHANGELOG.md
 
-## Report a bug
-
-https://github.com/YunoHost-Apps/shaarli_ynh/issues
-
 #### Supported architectures
 
-* x86-64b - [![Build Status](https://ci-apps.yunohost.org/ci/logs/shaarli%20%28Community%29.svg)](https://ci-apps.yunohost.org/ci/apps/shaarli/)
-* ARMv8-A - [![Build Status](https://ci-apps-arm.yunohost.org/ci/logs/shaarli%20%28Community%29.svg)](https://ci-apps-arm.yunohost.org/ci/apps/shaarli/)
-* Jessie x86-64b - [![Build Status](https://ci-stretch.nohost.me/ci/logs/shaarli%20%28Community%29.svg)](https://ci-stretch.nohost.me/ci/apps/shaarli/)
+* x86-64 - [![Build Status](https://ci-apps.yunohost.org/ci/logs/shaarli%20%28Apps%29.svg)](https://ci-apps.yunohost.org/ci/apps/shaarli/)
+* ARMv8-A - [![Build Status](https://ci-apps-arm.yunohost.org/ci/logs/shaarli%20%28Apps%29.svg)](https://ci-apps-arm.yunohost.org/ci/apps/shaarli/)
 
+## Limitations
+
+* Any known limitations.
+
+## Additional information
+
+## Links
+
+ * Report a bug: https://github.com/YunoHost-Apps/shaarli_ynh/issues
+ * App website: https://shaarli.readthedocs.io/
+ * Upstream app repository: https://github.com/shaarli/Shaarli
+ * YunoHost website: https://yunohost.org/
+
+---
+
+## Developer info
+
+Please send your pull request to the [testing branch](https://github.com/YunoHost-Apps/shaarli_ynh/tree/testing).
+
+To try the testing branch, please proceed like that.
+```
+sudo yunohost app install https://github.com/YunoHost-Apps/shaarli_ynh/tree/testing --debug
+or
+sudo yunohost app upgrade shaarli -u https://github.com/YunoHost-Apps/shaarli_ynh/tree/testing --debug
+```
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,91 +1,38 @@
 #!/bin/bash
 
-# ============= FUTURE YUNOHOST HELPER =============
-# Delete a file checksum from the app settings
-#
-# $app should be defined when calling this helper
-#
-# usage: ynh_remove_file_checksum file
-# | arg: file - The file for which the checksum will be deleted
-ynh_delete_file_checksum () {
-	local checksum_setting_name=checksum_${1//[\/ ]/_}	# Replace all '/' and ' ' by '_'
-	ynh_app_setting_delete $app $checksum_setting_name
-}
-
 #=================================================
 # COMMON VARIABLES
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="php-cli php-gettext php-curl php-intl php-gd php-mbstring openssl"
+YNH_PHP_VERSION="7.3"
 
+# dependencies used by the app
+pkg_dependencies=""
+
+extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gettext php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
 
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
 
-# Create a dedicated fail2ban config (jail and filter conf files)
+# Check available space before creating a temp directory.
 #
-# usage: ynh_add_fail2ban_config log_file filter [max_retry [ports]]
-# | arg: log_file - Log file to be checked by fail2ban
-# | arg: failregex - Failregex to be looked for by fail2ban
-# | arg: max_retry - Maximum number of retries allowed before banning IP address - default: 3
-# | arg: ports - Ports blocked for a banned IP address - default: http,https
-ynh_add_fail2ban_config_temp () {
-   # Process parameters
-   logpath=$1
-   failregex=$2
-   max_retry=${3:-3}
-   ports=${4:-http,https}
-   
-  test -n "$logpath" || ynh_die "ynh_add_fail2ban_config expects a logfile path as first argument and received nothing."
-  test -n "$failregex" || ynh_die "ynh_add_fail2ban_config expects a failure regex as second argument and received nothing."
-  
-  finalfail2banjailconf="/etc/fail2ban/jail.d/$app.conf"
-  finalfail2banfilterconf="/etc/fail2ban/filter.d/$app.conf"
-  ynh_backup_if_checksum_is_different "$finalfail2banjailconf" 1
-  ynh_backup_if_checksum_is_different "$finalfail2banfilterconf" 1
-  
-  tee $finalfail2banjailconf <<EOF
-[$app]
-enabled = true
-port = $ports
-filter = $app
-logpath = $logpath
-maxretry = $max_retry
-EOF
-
-  tee $finalfail2banfilterconf <<EOF
-[INCLUDES]
-before = common.conf
-[Definition]
-failregex = $failregex
-ignoreregex =
-EOF
-
-  ynh_store_file_checksum "$finalfail2banjailconf"
-  ynh_store_file_checksum "$finalfail2banfilterconf"
-  
-  service fail2ban restart
-  local fail2ban_error="$(journalctl -u fail2ban | tail -n50 | grep "WARNING.*$app.*")"
-  if [ -n "$fail2ban_error" ]
-  then
-    echo "[ERR] Fail2ban failed to load the jail for $app" >&2
-    echo "WARNING${fail2ban_error#*WARNING}" >&2
-  fi
-}
-
-# Remove the dedicated fail2ban config (jail and filter conf files)
+# usage: ynh_smart_mktemp --min_size="Min size"
 #
-# usage: ynh_remove_fail2ban_config
-ynh_remove_fail2ban_config () {
-  ynh_secure_remove "/etc/fail2ban/jail.d/$app.conf"
-  ynh_secure_remove "/etc/fail2ban/filter.d/$app.conf"
-  systemctl restart fail2ban
-}
-
+# | arg: -s, --min_size= - Minimal size needed for the temporary directory, in Mb
 ynh_smart_mktemp () {
-        local min_size="${1:-300}"
+        # Declare an array to define the options of this helper.
+        declare -Ar args_array=( [s]=min_size= )
+        local min_size
+        # Manage arguments with getopts
+        ynh_handle_getopts_args "$@"
+
+        min_size="${min_size:-300}"
         # Transform the minimum size from megabytes to kilobytes
         min_size=$(( $min_size * 1024 ))
 
@@ -104,8 +51,152 @@ ynh_smart_mktemp () {
         elif is_there_enough_space /home; then
                 local tmpdir=/home
         else
-		ynh_die "Insufficient free space to continue..."
+                ynh_die "Insufficient free space to continue..."
         fi
 
         echo "$(mktemp --directory --tmpdir="$tmpdir")"
 }
+
+# Create a dedicated config file from a template
+#
+# examples:
+#    ynh_add_config --template=".env" --destination="$final_path/.env"
+#    ynh_add_config --template="../conf/.env" --destination="$final_path/.env"
+#    ynh_add_config --template="/etc/nginx/sites-available/default" --destination="etc/nginx/sites-available/mydomain.conf"
+#
+# usage: ynh_add_config --template="template" --destination="destination"
+# | arg: -t, --template=     - Template config file to use
+# | arg: -d, --destination=    - Destination of the config file
+#
+# The template can be by default the name of a file in the conf directory
+# of a YunoHost Package, a relative path or an absolute path
+# The helper will use the template $template to generate a config file
+# $destination by replacing the following keywords with global variables
+# that should be defined before calling this helper :
+#   __PATH__             by  $path_url
+#   __NAME__            by $app
+#   __NAMETOCHANGE__    by $app
+#   __USER__            by $app
+#   __FINALPATH__       by $final_path
+#   __PHPVERSION__      by $YNH_PHP_VERSION
+#
+# And any dynamic variables that should be defined before calling this helper like:
+#   __DOMAIN__   by $domain
+#   __APP__      by $app
+#   __VAR_1__    by $var_1
+#   __VAR_2__    by $var_2
+#
+# The helper will verify the checksum and backup the destination file
+# if it's different before applying the new template.
+# And it will calculate and store the destination file checksum
+# into the app settings when configuration is done.
+#
+#
+ynh_add_config () {
+    # Declare an array to define the options of this helper.
+    local legacy_args=tdv
+    local -A args_array=( [t]=template= [d]=destination= )
+    local template
+    local destination
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+    local template_path
+
+    if [ -f "../conf/$template" ]; then
+        template_path="../conf/$template"
+    elif [ -f "../settings/conf/$template" ]; then
+        template_path="../settings/conf/$template"
+    elif [ -f "$template" ]; then
+        template_path=$template
+    else
+        ynh_die --message="The provided template $template doesn't exist"
+    fi
+
+    ynh_backup_if_checksum_is_different --file="$destination"
+
+    cp "$template_path" "$destination"
+
+    ynh_replace_vars --file="$destination"
+
+    ynh_store_file_checksum --file="$destination"
+}
+
+# Replace variables in a file
+#
+# usage: ynh_replace_vars --file="file"
+# | arg: -f, --file=     - File where to replace variables
+#
+# The helper will replace the following keywords with global variables
+# that should be defined before calling this helper :
+#   __PATH__             by  $path_url
+#   __NAME__            by $app
+#   __NAMETOCHANGE__    by $app
+#   __USER__            by $app
+#   __FINALPATH__       by $final_path
+#   __PHPVERSION__      by $YNH_PHP_VERSION
+#
+# And any dynamic variables that should be defined before calling this helper like:
+#   __DOMAIN__   by $domain
+#   __APP__      by $app
+#   __VAR_1__    by $var_1
+#   __VAR_2__    by $var_2
+#
+#
+ynh_replace_vars () {
+    # Declare an array to define the options of this helper.
+    local legacy_args=f
+    local -A args_array=( [f]=file= )
+    local file
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+
+    # Replace specific YunoHost variables
+    if test -n "${path_url:-}"
+    then
+        # path_url_slash_less is path_url, or a blank value if path_url is only '/'
+        local path_url_slash_less=${path_url%/}
+        ynh_replace_string --match_string="__PATH__/" --replace_string="$path_url_slash_less/" --target_file="$file"
+        ynh_replace_string --match_string="__PATH__" --replace_string="$path_url" --target_file="$file"
+    fi
+    if test -n "${app:-}"; then
+        ynh_replace_string --match_string="__NAME__" --replace_string="$app" --target_file="$file"
+        ynh_replace_string --match_string="__NAMETOCHANGE__" --replace_string="$app" --target_file="$file"
+        ynh_replace_string --match_string="__USER__" --replace_string="$app" --target_file="$file"
+    fi
+    if test -n "${final_path:-}"; then
+        ynh_replace_string --match_string="__FINALPATH__" --replace_string="$final_path" --target_file="$file"
+    fi
+    if test -n "${YNH_PHP_VERSION:-}"; then
+        ynh_replace_string --match_string="__PHPVERSION__" --replace_string="$YNH_PHP_VERSION" --target_file="$file"
+    fi
+    if test -n "${ynh_node_load_PATH:-}"; then
+        ynh_replace_string --match_string="__YNH_NODE_LOAD_PATH__" --replace_string="$ynh_node_load_PATH" --target_file="$file"
+    fi
+
+    # Replace others variables
+
+    # List other unique (__ __) variables in $file
+    local uniques_vars=( $(grep -o '__[A-Z0-9_]*__' $file | sort --unique | sed "s@__\([^.]*\)__@\L\1@g" ))
+
+    # Do the replacement
+    local delimit=@
+    for one_var in "${uniques_vars[@]}"
+    do
+        # Validate that one_var is indeed defined
+        test -n "${!one_var:-}" || ynh_die --message="\$$one_var wasn't initialized when trying to replace __${one_var^^}__ in $file"
+
+        # Escape delimiter in match/replace string
+        match_string="__${one_var^^}__"
+        match_string=${match_string//${delimit}/"\\${delimit}"}
+        replace_string="${!one_var}"
+        replace_string=${replace_string//${delimit}/"\\${delimit}"}
+
+        # Actually replace (sed is used instead of ynh_replace_string to avoid triggering an epic amount of debug logs)
+        sed --in-place "s${delimit}${match_string}${delimit}${replace_string}${delimit}g" "$file"
+    done
+}
+
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
