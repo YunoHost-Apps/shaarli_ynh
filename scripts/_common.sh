06dcc36382f22ff27aed16d9592b111bb1679f0f
ericgaspar <junk.eg@free.fr>
2020-12-01 12:05:35 +0100

    Remove dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,9 +7,6 @@
 # dependencies used by the app
 YNH_PHP_VERSION="7.3"
 
-# dependencies used by the app
-pkg_dependencies=""
-
 extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gettext php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring"
 
 #=================================================
